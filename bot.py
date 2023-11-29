faac99393b8c916fb643a1ee76fb3601220e695d
lanetb <lanetb@dukes.jmu.edu>
2023-11-29 17:19:39 -0500

    testing phase

--- bot.py
+++ bot.py
@@ -1,3 +1,38 @@
 import discord
 import responses
+import os
 
+async def send_message(message):
+    try:
+        response = responses.handle_message(message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    print(os.getenv('TOKEN'))
+    TOKEN = os.getenv('TOKEN')
+    client = discord.Client(intents=discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        print(user_message)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        if user_message[0] == "!":
+            user_message = user_message[1:]
+            await send_message(message)
+
+    client.run(TOKEN)
+    
--- responses.py
+++ responses.py
@@ -1,9 +1,10 @@
 import discord
+import random
+import re
 
 class Quotes_Chat:
     def __init__(self, quotes_chat, quote_of_the_day):
         self.quotes_chat = quotes_chat
-        self.quotes = []
         self.quotes_of_the_day = quote_of_the_day
 
     def set_quotes_chat(self, quotes_chat):
@@ -12,33 +13,51 @@ class Quotes_Chat:
     def get_quotes_chat(self):
         return self.quotes_chat
     
-    def set_quotes(self, quotes):
-        self.quotes = quotes
-    
-    def get_quotes(self):
-        return self.quotes
-    
     def set_quotes_of_the_day(self, quotes_of_the_day):
         self.quotes_of_the_day = quotes_of_the_day
 
     def get_quotes_of_the_day(self):
         return self.quotes_of_the_day
     
-quotes_chat = Quotes_Chat(None)
+quotes_chat = Quotes_Chat(None, None)
 
 def handle_message(message) -> str:
     p_message = message.lower()
 
-    if p_message == "$hi":
+    if p_message == "hi":
         return "Hello, how are you?"
     
-    if p_message == "$quote" or p_message == "$q":
-        return "place holder"
-    
-    if p_message == "$setquoteschat" or p_message == "$sqc":
+    if p_message == "quote" or p_message == "q":
+        if quotes_chat.get_quotes_chat() == None:
+            return "No quotes chat has been set."
+        else:
+            try:
+                list_q = quotes_chat.get_quotes_chat().history()
+                return get_random_quote(list_q)
+            except quotes_chat.get_quotes_chat.history().Forbidden:
+                return "I don't have permissions for that chat."
+            except quotes_chat.get_quotes_chat.history().HTTPException:
+                return "Could not get the chat history."
+            
+    if p_message == "setquoteschat" or p_message == "sqc":
         quotes_chat.set_quotes_chat(discord.TextChannel)
         return "This chat has been set as the quotes chat."
     
-def quote_of_the_day() -> str:
-    return "place holder"
+    if p_message == "$setquotesoftheday" or p_message == "$sqotd":
+        quotes_chat.set_quotes_of_the_day(discord.TextChannel)
+        
+
+def get_random_quote(qoutes):
+    regex = re.compile('"[A-Za-z0-9\s]+" - [A-Za-z0-9@\s]+')
+    qoute = random.choice(qoutes)
+    if re.match(regex, qoute):
+        return qoute
+    else:
+        return get_random_quote(qoutes)
+    
+def parse_quote(quote):
+    regex = re.compile('"[A-Za-z0-9\s]+"')
+    q = re.split(regex, quote)
+    regex = re.compile('@')
+
 
